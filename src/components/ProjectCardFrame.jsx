1fa47613cd923cd3784a2a1ac4c191248f481a2d
Hashir <hashirshah222@gmail.com>
2023-08-26 13:28:10 -0500

    Added new project and updated skills and resume

--- src/components/ProjectCardFrame.jsx
+++ src/components/ProjectCardFrame.jsx
@@ -13,9 +13,9 @@ const ProjectCardFrame = () => {
 
       <ProjectCard
         title='Learners Loft'
-        body={<p>Through the fusion of Firebase, React.js, and Node.js, I've sculpted a dynamic and engaging blog website called <a href="http://www.learnersloft.org">Learners Loft</a>, that stands as a testament to my skill set in full-stack development. The user experience is at the forefront, manifested through a meticulously crafted front-end developed with React. This design ensures seamless navigation, regardless of device, accessibility, and user satisfaction. CSS is used to make the site responsive and performant on various browsers and devices. The backend is built using Node.js using Google Firebase Firestore as the primary storage solution, guaranteeing efficient data management. The site is deployed using Netlify directly from GitHub, where the <a href="https://github.com/HashMaster02/learners-loft-website">code</a> can be found.</p>}
-        date="July-August, 2023"
-        tagList={['React', 'Node', 'CSS', 'Firebase', 'Netlify']}
+        body={<p>Through the fusion of Firebase, React.js, and Node.js, I've sculpted a dynamic and engaging blog website called <a href="http://www.learnersloft.org">Learners Loft</a>, that stands as a testament to my skill set in full-stack development. The user experience is at the forefront, manifested through a meticulously crafted design in Figma, and a front-end developed with React. This design ensures seamless navigation, regardless of device, accessibility, and user satisfaction. CSS is used to make the site responsive and performant on various browsers and devices. The backend is built using Node.js using Google Firebase Firestore as the primary storage solution, guaranteeing efficient data management. The site is deployed using Netlify directly from GitHub, where the <a href="https://github.com/HashMaster02/learners-loft-website">code</a> can be found.</p>}
+        date="June, 2023 - August, 2023"
+        tagList={['React', 'Node', 'CSS', 'Firebase', 'Netlify', 'Figma']}
       />
 
     </div>
