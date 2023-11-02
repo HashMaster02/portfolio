c06dc27a46381d75cb96417c131f6eb7ea0eb86f
Hashir <70011762+HashMaster02@users.noreply.github.com>
2023-11-02 12:30:00 -0500

    Removed padding from '.section', lowered size of fonts on mobile.

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,17 +1,42 @@
-import { FaEnvelope, FaPhone, FaLinkedin, FaSquareGithub } from 'react-icons/fa6'
-import ContactCard from './ContactCard'
+import {
+  FaEnvelope,
+  FaPhone,
+  FaLinkedin,
+  FaSquareGithub,
+} from "react-icons/fa6";
+import ContactCard from "./ContactCard";
 
 const ContactInfo = () => {
-    return (
-        <div className="contact-frame">
-            <ContactCard icon={<FaEnvelope size={40} />} children={['hashirshah222@gmail.com']} />
-            <ContactCard icon={<FaPhone size={40} />} children={['US: 312-998-5441', 'UK: 07933 119784']} />
+  return (
+    <div className="contact-frame">
+      <ContactCard
+        icon={<FaEnvelope size={40} />}
+        children={["hshah86@hawk.iit.edu"]}
+      />
+      <ContactCard
+        icon={<FaPhone size={40} />}
+        children={["US: 312-998-5441", "UK: 07933 119784"]}
+      />
 
-            <div className="contact-links">
-                <a href="https://www.linkedin.com/in/hashirshah/" target='_blank' rel='noreferrer'> <FaLinkedin size={80} /> </a>
-                <a href="https://github.com/HashMaster02" target='_blank' rel='noreferrer'> <FaSquareGithub size={80} /> </a>
-            </div>
-        </div>
-    )
-}
-export default ContactInfo
+      <div className="contact-links">
+        <a
+          href="https://www.linkedin.com/in/hashirshah/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <FaLinkedin size={80} />{" "}
+        </a>
+        <a
+          href="https://github.com/HashMaster02"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <FaSquareGithub size={80} />{" "}
+        </a>
+      </div>
+    </div>
+  );
+};
+export default ContactInfo;
